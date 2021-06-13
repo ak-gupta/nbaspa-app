5b29b0da3c33a09e56956a65d89a809e5e52e8d1
Akshay Gupta <akshaykgupta@protonmail.com>
2021-06-13 11:43:06 -0400

    Broken but first attempt at hover tooltips

--- nbaspa_app/games/static/dist/js/game-graph.js
+++ nbaspa_app/games/static/dist/js/game-graph.js
@@ -1,3 +1,10 @@
+/**
+ * Creates a D3.js visualization for game win probability.
+ * @function drawGameChart
+ * @param {Array} data The event-level game data
+ * @param {string} tag The HTML div ID for the graph
+ */
+
 function drawGameChart(data, tag) {
     var margin = {
         top: 20,
@@ -18,17 +25,8 @@ function drawGameChart(data, tag) {
         )
     )
     const y = d3.scaleLinear().domain([0, 1]).range([graphHeight, 0])
-    const wProbAxis = d3.axisLeft(y).ticks(5).tickFormat(d3.format("~%"))
-    const scoreMargin = d3.scaleLinear().range([graphHeight, 0])
-    scoreMargin.domain(
-        d3.extent(
-            data,
-            function (d) {
-                return d.SCOREMARGIN
-            }
-        )
-    )
-    const marginAxis = d3.axisRight(scoreMargin)
+    const percentFormat = d3.format("~%")
+    const wProbAxis = d3.axisLeft(y).ticks(5).tickFormat(percentFormat)
     // Create the win probability line
     var winLine = d3.line().x(
         function (d) {
@@ -37,16 +35,6 @@ function drawGameChart(data, tag) {
     ).y(
         function (d) {
             return y(d.WIN_PROB)
-        }
-    )
-    // Create the margin line
-    var marginLine = d3.line().x(
-        function (d) {
-            return x(d.TIME)
-        }
-    ).y(
-        function (d) {
-            return scoreMargin(d.SCOREMARGIN)
         }
     )
     // Create the SVG
@@ -59,23 +47,66 @@ function drawGameChart(data, tag) {
         .attr("transform", "translate(0," + graphHeight + ")")
         .call(d3.axisBottom(x))
     svg.append("g").call(wProbAxis)
-    svg.append("g")
-        .attr("transform", "translate(" + graphWidth + ")")
-        .call(marginAxis)
     // Add the Win Probability line
     svg.append("path")
         .data([data])
         .attr("class", "line")
         .attr("d", winLine)
-        .style("stroke", "blue")
-        .attr("stroke-width", 2)
-        .style("fill", "none")
-    // Add the Margin line
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", marginLine)
         .style("stroke", "black")
         .attr("stroke-width", 1)
         .style("fill", "none")
+    // Add dots with mouseover
+    var div = d3.select(tag).append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+    svg.selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("r", 4)
+        .attr(
+            "cx",
+            function (d) {
+                return x(d.TIME)
+            }
+        )
+        .attr(
+            "cy",
+            function (d) {
+                return y(d.WIN_PROB)
+            }
+        )
+        .attr("stroke", "black")
+        .attr("stroke-width", 1)
+        .attr("fill", "#FFFFFF")
+        .on(
+            "mouseover",
+            function (d) {
+                d3.select(this)
+                    .transition()
+                    .duration("100")
+                    .attr("r", 6)
+                // Make the div appear
+                div.transition()
+                    .duration("100")
+                    .style("opacity", 1)
+                // Add data
+                div.html(
+                    "Win Probability: " + d.WIN_PROB
+                )
+            }
+        )
+        .on(
+            "mouseout",
+            function (d, i) {
+                d3.select(this)
+                    .transition()
+                    .duration("200")
+                    .attr("r", 4)
+                // Make div disappear
+                div.transition()
+                    .duration("200")
+                    .style("opacity", 0)
+            }
+        )
 }
