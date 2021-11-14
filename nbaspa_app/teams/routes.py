2a7390a4cdc56f31b726383757643130f173ff49
Akshay Gupta <akgcodes@gmail.com>
2021-11-13 19:30:34 -0500

    Deleting legacy code

--- nbaspa_app/teams/routes.py
+++ nbaspa_app/teams/routes.py
@@ -3,11 +3,6 @@
 from flask import Blueprint, render_template
 from flask import current_app as app
 
-from .data import (
-    gen_teamlist,
-    gen_roster
-)
-
 teams_bp = Blueprint(
     "teams_bp",
     __name__,
@@ -48,27 +43,3 @@ def team_season_summary(teamid: int, season: int):
         The season.
     """
     return render_template("teamseason.html", teamid=teamid, season=season)
-
-
-@teams_bp.get("/teams/<int:teamid>/<season>/players")
-def team_players(teamid: int, season: int):
-    """The team roster.
-
-    Parameters
-    ----------
-    team_id : int
-        The team identifier.
-    season : int
-        The season year.
-    """
-    teamlist = gen_teamlist(app=app)
-    teamname = [row["teamname"] for row in teamlist if row["teamid"] == teamid][0]
-    roster = gen_roster(app=app, teamid=teamid, season=season)
-    return render_template(
-        "players.html",
-        title=f"{season} Roster",
-        teamid=teamid,
-        teamname=teamname,
-        season=season,
-        data=[roster[i:i+3] for i in range(0, len(roster), 3)]
-    )
