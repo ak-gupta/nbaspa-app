1361b18de2c232d30d32a9c927140ffcbb466d45
Akshay Gupta <akgcodes@gmail.com>
2021-11-16 00:46:35 -0500

    For some reason it can't find the javascript file...

---
+++ nbaspa_app/home/static/dist/js/tracker.js
@@ -0,0 +1,7 @@
+/**
+ * @module tracker The homepage MVP tracker
+ */
+
+const mvp = new AwardList(Season, "/api/league/mvp", "/league/mvp/", "survival-plus", "mean")
+mvp.loadData()
+mvp.updateList()
