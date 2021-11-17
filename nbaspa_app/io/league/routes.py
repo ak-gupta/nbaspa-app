003c3dfc4823ff0b3b9c0ec2ec8fd970ed854d6a
Akshay Gupta <30963644+ak-gupta@users.noreply.github.com>
2021-11-16 23:03:26 -0500

    Creating a homepage with the MVP tracker (#13)
    * Tweaks to the organization of the season page
    
    * Starting point for a homepage
    
    * For some reason it can't find the javascript file...
    
    * Found out why Javascript wasn't loading -- ad blocker wasn't letting flask load the file
    
    * Small edit to the homepage
    
    * Font awesome and some sort of footer

--- nbaspa_app/io/league/routes.py
+++ nbaspa_app/io/league/routes.py
@@ -9,8 +9,8 @@ from flask_smorest import Blueprint, abort
 import numpy as np
 import pandas as pd
 
-from nbaspa.data.endpoints import Scoreboard, AllPlayers
-from nbaspa.data.endpoints.parameters import CURRENT_SEASON, SEASONS, Season
+from nbaspa.data.endpoints import AllPlayers
+from nbaspa.data.endpoints.parameters import Season
 
 
 from . import schemas as sc
@@ -18,38 +18,6 @@ from . import schemas as sc
 io_league = Blueprint(
     "io_league", __name__, url_prefix="/api/league", description="Load league data"
 )
-
-@io_league.route("/standings")
-class Standings(MethodView):
-    """Load the latest available standings."""
-
-    @io_league.arguments(sc.StandingsQueryArgsSchema, location="query")
-    @io_league.response(200, sc.StandingsOutputSchema(many=True))
-    def get(self, args):
-        """Retrieve the standings."""
-        if args["Season"] == CURRENT_SEASON:
-            gdate = datetime.today().strftime("%m/%d/%Y")
-        else:
-            gdate = SEASONS[args["Season"]]["END"].strftime("%m/%d/%Y")
-        
-        loader = Scoreboard(
-            output_dir=Path(app.config["DATA_DIR"], args["Season"]),
-            filesystem=app.config["FILESYSTEM"],
-            Season=args["Season"],
-            GameDate=gdate
-        )
-        if not loader.exists() and args["Season"] == CURRENT_SEASON:
-            loader.params["GameDate"] = (datetime.today() - timedelta(days=1)).strftime("%m/%d/%Y")
-        if not loader.exists():
-            abort(404, message="Unable to load the scoreboard information.")
-        # Get the conference standings
-        loader.load()
-        data = loader.get_data(
-            "EastConfStandingsByDay" if args["Conference"] == "east" else "WestConfStandingsByDay"
-        )
-
-        return data[["TEAM_ID", "TEAM", "G", "W", "L", "W_PCT"]].to_dict(orient="records")
-
 
 @io_league.route("/mvp")
 class TopPlayers(MethodView):
--- nbaspa_app/io/league/schemas.py
+++ nbaspa_app/io/league/schemas.py
@@ -2,24 +2,7 @@
 
 from marshmallow import Schema, fields, validate
 
-from nbaspa.data.endpoints.parameters import CURRENT_SEASON, SEASONS, ParameterValues
-
-class StandingsQueryArgsSchema(Schema):
-    Season = fields.String(
-        validate=validate.OneOf(list(SEASONS.keys())),
-        default=CURRENT_SEASON
-    )
-    Conference = fields.String(validate=validate.OneOf(["east", "west"]), required=True)
-
-class StandingsOutputSchema(Schema):
-    TEAM_ID = fields.String(
-        validate=validate.OneOf(list(ParameterValues().TeamID))
-    )
-    TEAM = fields.String()
-    G = fields.Int()
-    W = fields.Int()
-    L = fields.Int()
-    W_PCT = fields.Float()
+from nbaspa.data.endpoints.parameters import CURRENT_SEASON
 
 class SummaryQueryArgsSchema(Schema):
     Season = fields.String(default=CURRENT_SEASON)
